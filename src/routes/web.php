02195137bc36762320deefe3ea5487e238426918
Arkaitz Amostegui <ikdge@plaiaundi.net>
2025-11-03 17:59:40 +0100

    Docker-compose, vite, instalción en rama casa

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\PalabraController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta que devuelve todas las palabras de la tabla 'palabras'
+Route::get('/palabras', [PalabraController::class, 'index'])->name('palabras.index');
+
+//Ruta que devuelve todas las palabras de la tabla 'palabras' con estilos css
+Route::get('/palabrasStyled', [PalabraController::class, 'indexStyled'])->name('palabras.index');
+
+//Ruta que devuelve todas las palabras de la tabla 'palabras' con estilos css
+Route::get('/palabrasBlade', [PalabraController::class, 'indexBlade'])->name('palabras.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
